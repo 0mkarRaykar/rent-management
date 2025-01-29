201ce81e6d9b15b5c05f3f3cc1fe5e6564c03857
Omkar Raykar <raykaromkar1244@gmail.com>
2025-01-29 20:46:19 +0530

    add proxy service

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
+    jsxFactory: "h",
+    jsxFragment: "Fragment",
   },
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
+      "/api": {
+        target:
+          "http://localhost:8000" ||
+          "https://rent-management-pg2q.onrender.com",
         changeOrigin: true,
       },
     },
   },
-})
+});
