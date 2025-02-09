604c638978d1708d513440318942d991f0659487
Omkar Raykar <raykaromkar1244@gmail.com>
2025-02-09 15:46:39 +0530

    code to toggle active status and create user

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -6,10 +6,53 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 import { User } from "../models/userModel.js";
 
+// @desc     create new user (role based)
+// route     POST api/v1/users/createUser
+// @accesss  Private
+const createUser = asyncHandler(async (req, res) => {
+  const { email, password, fullName, mobileNumber, address } = req.body;
+
+  const allowedRoles = [0];
+  if (!allowedRoles.includes(req.user.role)) {
+    return res.status(403).json({
+      success: false,
+      message: "Access denied: Unauthorized to access this resource",
+    });
+  }
+
+  if (!email || !password || !fullName || !mobileNumber) {
+    return res.status(403).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+
+  const user = await User.create({
+    email,
+    password,
+    fullName,
+    mobileNumber,
+    address
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while creating the user",
+    });
+  }
+  // respond with the created user
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User created successfully"));
+});
+
 // @desc     fetch all user from db (role based fetching)
 // route     GET api/v1/users/getAllUsers
 // @accesss  Private
-
 const getAllUsers = asyncHandler(async (req, res) => {
   try {
     const requestingUser = await User.findById(req.user._id);
@@ -248,4 +291,71 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { getAllUsers, getUserById, updateUser, deleteUser };
+// @desc     toggle user status(role based)
+// route     PATCH api/v1/users/toggleActiveStatus/{id}
+// @accesss  Private
+const toggleActiveStatus = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  // Validate the userId parameter
+  if (!isValidObjectId(userId)) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid user ID",
+    });
+  }
+  try {
+    // Get the requesting user's details
+    const requestingUser = await User.findById(req.user._id);
+
+    if (!requestingUser) {
+      return res.status(404).json({
+        success: false,
+        message: "Requesting user not found",
+      });
+    }
+
+    // Check if the requesting user has role 0 (Admin)
+    if (![0].includes(requestingUser.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied: Unauthorized to delete this user",
+      });
+    }
+
+    // Find and update the user using atomic operation
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      [{ $set: { isActive: { $not: "$isActive" } } }],
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Active status toggled successfully",
+      data: updatedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error toggling user status",
+      error: error.message,
+    });
+  }
+});
+
+export {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  toggleActiveStatus,
+};
--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -2,16 +2,21 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/authMiddleware.js";
 
 import {
+  createUser,
   getAllUsers,
   getUserById,
   updateUser,
   deleteUser,
+  toggleActiveStatus
 } from "../controllers/userController.js";
 
 const router = Router();
 router.use(verifyJWT);
 
+router.route("/createUser").post(verifyJWT, createUser);
 router.route("/getAllUsers").get(verifyJWT, getAllUsers);
 router.route("/:userId").get(getUserById).patch(updateUser).delete(deleteUser);
+
+router.patch("/toggleActiveStatus/:userId", toggleActiveStatus);
 
 export default router;
